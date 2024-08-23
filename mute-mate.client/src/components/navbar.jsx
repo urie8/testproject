5dbc5c45d61af012589d04b908232592efd12b1a
pelicanpubis <norah.binsalamah@hotmail.com>
2024-08-23 11:16:21 +0200

    har skapat en navbar med en hamburgar meny för mobil versionen

--- mute-mate.client/src/components/navbar.jsx
+++ mute-mate.client/src/components/navbar.jsx
@@ -1,7 +1,115 @@
+// import React from "react";
+// import { NavLink } from "react-router-dom";
+// import logo from "../images/MuteMateLogo.png";
+// import "../navbar.css";
+// import Hamburger from "hamburger-react";
+// import { useClickAway } from "react-use";
+// import { useRef } from "react";
+// import { useState } from "react";
+// import { AnimatePresence, motion } from "framer-motion";
+
+// function Navbar() {
+//   const [isOpen, setOpen] = useState(false);
+
+//   const ref = useRef(null); //använder en hook från React-use som kollar om en användare har klickat utan för menyn.
+
+//   useClickAway(ref, () => setOpen(false));
+
+//   return (
+//     <>
+//       <div className="navbar-wrapper">
+//         <img src={logo} alt="Logo" className="navbar-logo" />
+//         <div className="mute-mate-text">MUTE MATE</div>
+//           <Hamburger className= "hamburger-react" />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Navbar;
+
+import React from "react";
+import { NavLink } from "react-router-dom";
+import logo from "../images/MuteMateLogo.png";
+import vines from "../images/vines.png";
+
+import "../navbar.css";
+import Hamburger from "hamburger-react";
+import { useClickAway } from "react-use";
+import { useRef } from "react";
+import { useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
 function Navbar() {
+  const [isOpen, setOpen] = useState(false);
+
+  const ref = useRef(null);
+
+  // Close the menu when clicking outside of it
+  useClickAway(ref, () => setOpen(false));
+
   return (
     <>
-      <div>Hej</div>
+      <div className="navbar-wrapper" ref={ref}>
+        <NavLink to="/">
+          <img src={logo} alt="Logo" className="navbar-logo" />
+        </NavLink>
+        {/* <img src={logo} alt="Logo" className="navbar-logo" /> */}
+        <div className="mute-mate-text">MUTE MATE</div>
+        <Hamburger
+          toggled={isOpen}
+          toggle={setOpen}
+          className="hamburger-react"
+        />
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              className="dropdown-menu"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: -20 }}
+            >
+              <NavLink
+                to="/login"
+                className="dropdown-link"
+                onClick={() => setOpen(false)}
+              >
+                Register
+              </NavLink>
+              <NavLink
+                to="/categoryQuiz"
+                className="dropdown-link"
+                onClick={() => setOpen(false)}
+              >
+                Quiz
+              </NavLink>
+              <NavLink
+                to="/categoryPractise"
+                className="dropdown-link"
+                onClick={() => setOpen(false)}
+              >
+                Practice
+              </NavLink>
+              <NavLink
+                to="/about"
+                className="dropdown-link"
+                onClick={() => setOpen(false)}
+              >
+                About
+              </NavLink>
+              <NavLink
+                to="/myPage"
+                className="dropdown-link"
+                onClick={() => setOpen(false)}
+              >
+                Profile
+              </NavLink>
+
+              {/* TODO: profile ska bara synas om man är inloggad */}
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
     </>
   );
 }
