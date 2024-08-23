ff63e6426f29b2b7aa2226ac3c1ee8623fd859f6
pelicanpubis <norah.binsalamah@hotmail.com>
2024-08-23 15:23:52 +0200

    quiz api med ett repo

---
+++ mute-mate.Server/mute-mate.Server/Data/Repo/QuizRepo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MuteMate.Server.Data;
+using MuteMate.Server.Models;
+
+namespace mute_mate.Server.Data.Repo
+{
+    public class QuizRepo
+    {
+        private readonly MuteMateDbContext context;
+
+        public QuizRepo(MuteMateDbContext context)
+        {
+            this.context = context; 
+        }
+
+
+        public async Task<List<QuestionModel>> GetCategoryColor()
+        {
+           return await context.Questions.Where(q => q.Category == "Colors").Include( q=> q.Answers).ToListAsync();
+
+        }
+
+        public async Task<List<QuestionModel>> GetCategoryLetters()
+        {
+            return await context.Questions.Where(q => q.Category == "Letters").Include(q=> q.Answers).ToListAsync();
+
+        }
+
+        public async Task<List<QuestionModel>> GetCategoryAnimals()
+        {
+            return await context.Questions.Where(q => q.Category == "Animals").Include(q => q.Answers).ToListAsync();
+
+        }
+
+
+    }
+}
