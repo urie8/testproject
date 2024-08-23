061e56b066890ffa9a556a8a63df383e400d6a36
PaulineI <pauline.ibraim@gmail.com>
2024-08-23 14:01:52 +0200

    Farresdesignsynligigen

--- mute-mate.client/src/pages/home.jsx
+++ mute-mate.client/src/pages/home.jsx
@@ -7,31 +7,32 @@ function home() {
   const navigate = useNavigate();
   const handleRegisterClick = () => {
     navigate("/register");
-    return (
-      <>
-        <div className="container">
-          <div className="text-container">
-            <div>
-              <h1 className="title">
-                Mute <br />
-                Mate
-              </h1>
-            </div>
-            <div>
-              <h1 className="tagline">Say Less, Express More</h1>
-            </div>
+  };
+
+  return (
+    <>
+      <div className="container">
+        <div className="text-container">
+          <div>
+            <h1 className="title">
+              Mute <br />
+              Mate
+            </h1>
           </div>
-          <div className="img-homepage"></div>
-          <div className="button-container">
-            <button className="button">Play</button>
-            <button className="button" onClick={handleRegisterClick}>
-              Sign up
-            </button>
+          <div>
+            <h1 className="tagline">Say Less, Express More</h1>
           </div>
         </div>
-      </>
-    );
-  };
+        <div className="img-homepage"></div>
+        <div className="button-container">
+          <button className="button">Play</button>
+          <button className="button" onClick={handleRegisterClick}>
+            Sign up
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default home;
