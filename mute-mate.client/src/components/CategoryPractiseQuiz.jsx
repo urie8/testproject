ac8cf7713e0fa3abc4f7cab8f6a1d73580985ab0
miiamole <miia_pelaez@hotmail.com>
2024-08-26 14:17:55 +0200

    förbättrat bilderna

--- mute-mate.client/src/components/CategoryPractiseQuiz.jsx
+++ mute-mate.client/src/components/CategoryPractiseQuiz.jsx
@@ -2,23 +2,18 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "../categoryPractise.css";
 
-function CategoryPractiseQuiz({message, linkTo}) {
-    const [category, setCategory] = useState([]);
-    const navigate = useNavigate();
+function CategoryPractiseQuiz({ message, linkTo }) {
+  const [category, setCategory] = useState([]);
+  const navigate = useNavigate();
 
+  function handleImageClick() {
+    //Måste kolla vilken bild som klickats
 
- function handleImageClick() {
-   //Måste kolla vilken bild som klickats
-
-   navigate("/practice");
- }
+    navigate("/practice");
+  }
   return (
     <>
       <div className="page-wrapper">
-        {/* <div className="app-name-text">
-          <h1 className="head-text">Mute</h1>
-          <h1 className="head-text">Mate</h1>
-        </div> */}
         <h2 className="choose-text">{message}</h2>
         <div className="option-img-container">
           <img
