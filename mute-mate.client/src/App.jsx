7da38b46605f3e659c03a6eef1d2044cd392e217
urie8 <dionpeci08@gmail.com>
2024-08-27 09:51:10 +0200

    Component for quiz

--- mute-mate.client/src/App.jsx
+++ mute-mate.client/src/App.jsx
@@ -13,6 +13,7 @@ import LogIn from "./pages/login";
 import MyPage from "./pages/myPage";
 import Practise from "./pages/Practise";
 import Register from "./pages/register";
+import Quiz from "./pages/quiz";
 import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter(
@@ -25,6 +26,8 @@ const router = createBrowserRouter(
       <Route path="/categoryPractise" element={<CategoryPractise />} />
       <Route path="/categoryQuiz" element={<CategoryQuiz />} />
       <Route path="/practise" element={<Practise />} />
+      <Route path="/quiz/:category" element={<Quiz />} />
+
       <Route path="*" element={<NotFound />} />
     </Route>
   )
