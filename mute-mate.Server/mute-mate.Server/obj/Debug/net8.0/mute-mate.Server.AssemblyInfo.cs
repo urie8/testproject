5a73269c2c8dbd1369d63e9d04fda45ec090a349
PaulineI <pauline.ibraim@gmail.com>
2024-08-28 09:41:50 +0200

    Generic repo

--- mute-mate.Server/mute-mate.Server/obj/Debug/net8.0/mute-mate.Server.AssemblyInfo.cs
+++ mute-mate.Server/mute-mate.Server/obj/Debug/net8.0/mute-mate.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mute-mate.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddcf7f8ffeb041b2592b71502d7fcfc8b7f5d295")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92cb86e168bfaeda4a04199181f9d1c8c874ac98")]
 [assembly: System.Reflection.AssemblyProductAttribute("mute-mate.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mute-mate.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
